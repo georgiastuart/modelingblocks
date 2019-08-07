a59c449fa0f78e0bd65a28ed8f1752ea28964a32
Georgia Stuart <georgia.stuart@gmail.com>
2019-08-07 17:33:59 -0500

    added blockly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { BlocklyComponent } from './blockly/blockly.component';
+import { ConsoleComponent } from './console/console.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlocklyComponent,
+    ConsoleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
